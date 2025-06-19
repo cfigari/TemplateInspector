34f1ea5945634b4444711aee0710e887702bee99
cfigari <cfigari@gmail.com>
2025-06-18 20:00:30 -0400

    Sidebar compacto con tooltips y separación de extract-block component

--- src/app/shared/extract-block/extract-block.component.ts
+++ src/app/shared/extract-block/extract-block.component.ts
@@ -5,43 +5,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-extract-block',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <a href="javascript:void(0)" class="text-primary extract-link" title="Extraer {{ blockName }}" (click)="extractBlock()">
-      <i class="bi bi-box-arrow-up-right"></i>
-    </a>
-    
-    <!-- Modal para mostrar el bloque extraído -->
-    <div class="modal fade" [id]="modalId" tabindex="-1" [attr.aria-labelledby]="modalLabelId" aria-hidden="true">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" [id]="modalLabelId">{{ blockName }}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <pre class="bg-light p-3 rounded" style="max-height: 500px; overflow-y: auto;"><code>{{ blockContent }}</code></pre>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary" (click)="copyBlockContent()">
-              <i class="bi bi-clipboard"></i> Copiar Código
-            </button>
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
-  styles: [`
-    .extract-link {
-      text-decoration: none;
-      cursor: pointer;
-      margin-left: 5px;
-      font-size: 0.8rem;
-    }
-    .extract-link:hover {
-      text-decoration: underline;
-    }
-  `]
+  templateUrl: './extract-block.component.html',
+  styleUrl: './extract-block.component.css'
 })
 export class ExtractBlockComponent implements OnInit {
   @Input() blockName: string = '';
